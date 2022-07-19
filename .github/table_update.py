eb30b9cffe9a39e6c7f0e9854f8b59fa1b5d53ce
zeiosis <101180649+zeiosis@users.noreply.github.com>
2022-07-19 10:45:52 +0700

    reorganize files

--- .github/table_update.py
+++ .github/table_update.py
@@ -76,3 +76,9 @@ def update_table(filename, verbose=False):
     
     return sh_qu_MASTER
     #sh_qu_MASTER.to_csv(filename, index=False)
+
+
+
+
+x = update_table("SH_QU_0.csv")
+x.to_csv("SH_QU_0.csv", index=False)
